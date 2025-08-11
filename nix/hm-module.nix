648c9b90677f23d6b0eed981146dc4739783d0ce
Markus328 <markus328@tutanota.com>
2025-08-11 12:19:53 -0300

    nix/hm: remove uneeded options

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -4,143 +4,73 @@ self: {
   lib,
   ...
 }: let
-  caelestia-cli = self.inputs.caelestia-cli.packages.${pkgs.system}.default;
-  caelestia-shell = self.packages.${pkgs.system}.default;
-
-  caelestia-cli-ipc = "${caelestia-cli}/bin/caelestia shell";
-  caelestia-shell-ipc = "${caelestia-shell}/bin/caelestia-shell ipc call"; # Using this seems faster than caelestia-cli-ipc
+  shell-default = self.packages.${pkgs.system}.default;
 
   cfg = config.programs.caelestia;
 in {
   options = with lib; {
     programs.caelestia = {
       enable = mkEnableOption "Enable Caelestia shell";
-
+      package = mkOption {
+        type = types.package;
+        default = shell-default;
+        description = "The package of Caelestia shell";
+      };
       settings = mkOption {
         type = types.attrs;
         default = {};
-        description = "Caelestia settings written to shell.json";
+        description = "Caelestia shell settings";
       };
-
-      ipcType = mkOption {
-        type = types.enum [
-          "shell"
-          "cli"
-        ];
-        default = "shell";
-        description = "Caelestia IPC type, can be either \"shell\" or \"cli\", note that some commands may not work on both";
-      };
-
-      ipcCmd = mkOption {
+      extraConfig = mkOption {
         type = types.str;
-        description = "IPC command used in this module, configured by ipcType option";
-        readOnly = true;
-      };
-
-      binds = {
-        enable = mkEnableOption "Enable useful keybinds to Hyprland";
-        toggleDashboard = mkOption {
-          type = types.str;
-          description = "Hyprland bind to toggle the dashboard.";
-          example = "SUPER, D";
-          default = "";
-        };
-        toggleLauncher = mkOption {
-          type = types.str;
-          description = "Hyprland bind to toggle the launcher.";
-          example = "SUPER, D";
-          default = "";
-        };
-        toggleOsd = mkOption {
-          type = types.str;
-          description = "Hyprland bind to toggle the OSD.";
-          example = "SUPER, D";
-          default = "";
-        };
-        toggleBar = mkOption {
-          type = types.str;
-          description = "Hyprland bind to toggle the bar.";
-          example = "SUPER, D";
-          default = "";
-        };
-        openPicker = mkOption {
-          type = types.str;
-          description = "Hyprland bind to open the picker.";
-          example = "SUPER, D";
-          default = "";
-        };
-        openPickerFreeze = mkOption {
-          type = types.str;
-          description = "Hyprland bind to open the picker while freezing the screen.";
-          example = "SUPER, D";
-          default = "";
-        };
-        openControlCenter = mkOption {
-          type = types.str;
-          description = "Hyprland bind to open the Control Center.";
-          example = "SUPER, D";
-          default = "";
-        };
+        default = "";
+        description = "Caelestia shell extra configs written to shell.json";
       };
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.caelestia.ipcCmd =
-      if cfg.ipcType == "shell"
-      then caelestia-shell-ipc
-      else caelestia-cli-ipc;
+  config = let
+    shell = cfg.package or shell-default;
+  in
+    lib.mkIf cfg.enable {
+      systemd.user.services.caelestia = {
+        Unit = {
+          Description = "Caelestia Shell Service";
+          After = ["hyprland-session.target"];
+          Requires = ["hyprland-session.target"];
+          PartOf = ["graphical-session.target"];
+        };
 
-    wayland.windowManager.hyprland = {
-      settings = {
-        bind = let
-          bindIfConfigured = bindOpt: bindCmd: (lib.optional (bindOpt != "") "${bindOpt}, exec, ${cfg.ipcCmd} ${bindCmd}");
-        in
-          with cfg.binds;
-            lib.optionals cfg.binds.enable (
-              []
-              ++ bindIfConfigured toggleDashboard "drawers toggle dashboard"
-              ++ bindIfConfigured toggleLauncher "drawers toggle launcher"
-              ++ bindIfConfigured toggleOsd "drawers toggle osd"
-              ++ bindIfConfigured toggleBar "drawers toggle bar"
-              ++ bindIfConfigured openPicker "picker open"
-              ++ bindIfConfigured openPickerFreeze "picker openFreeze"
-              ++ bindIfConfigured openControlCenter "controlcenter open"
-            );
+        Service = {
+          ExecStart = "${shell}/bin/caelestia-shell";
+          Restart = "on-failure";
+          RestartSec = "5s";
+          Environment = [
+            "QT_QPA_PLATFORM=wayland"
+          ];
+
+          PassEnvironment = [
+            "WAYLAND_DISPLAY"
+            "XDG_RUNTIME_DIR"
+            "HYPRLAND_INSTANCE_SIGNATURE"
+          ];
+          Slice = "session.slice";
+        };
+
+        Install = {
+          WantedBy = ["hyprland-session.target"];
+        };
       };
+
+      home.file.".config/caelestia/shell.json".text = let
+        extraConfig =
+          if cfg.extraConfig != ""
+          then cfg.extraConfig
+          else "{}";
+      in
+        builtins.toJSON (lib.recursiveUpdate
+          (cfg.settings or {}) (builtins.fromJSON extraConfig));
+
+      home.packages = [shell];
     };
-
-    systemd.user.services.caelestia = {
-      Unit = {
-        Description = "Caelestia Shell Service";
-        After = ["hyprland-session.target"];
-        Requires = ["hyprland-session.target"];
-        PartOf = ["graphical-session.target"];
-      };
-
-      Service = {
-        ExecStart = "${caelestia-shell}/bin/caelestia-shell";
-        Restart = "on-failure";
-        RestartSec = "5s";
-        Environment = [
-          "QT_QPA_PLATFORM=wayland"
-        ];
-
-        PassEnvironment = [
-          "WAYLAND_DISPLAY"
-          "XDG_RUNTIME_DIR"
-          "HYPRLAND_INSTANCE_SIGNATURE"
-        ];
-        Slice = "session.slice";
-      };
-
-      Install = {
-        WantedBy = ["hyprland-session.target"];
-      };
-    };
-
-    home.file.".config/caelestia/shell.json".text = builtins.toJSON (cfg.settings or {});
-
-    home.packages = [caelestia-cli caelestia-shell];
-  };
 }
