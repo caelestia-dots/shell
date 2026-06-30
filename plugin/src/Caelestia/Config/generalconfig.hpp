d46fc97e6e03e1ca827db760fe68af2bf71efd8e
Chloé Legué <chloelegue@gmail.com>
2026-06-30 10:56:03 -0400

    fixing the C++ config

--- plugin/src/Caelestia/Config/generalconfig.hpp
+++ plugin/src/Caelestia/Config/generalconfig.hpp
@@ -86,7 +86,7 @@ class GeneralBattery : public settings::ObjectNode {
                     { u"level"_s, 90 },
                     { u"title"_s, u"Did you see the previous message?"_s },
                     { u"message"_s, u"You should probably unplug the charger <b>now</b>"_s },
-                    { u"icon"_s, u"battery_android_6"_s }
+                    { u"icon"_s, u"battery_android_6"_s },
                     { u"enabled"_s, true },
             }),
             vmap({
