eb97a0b1539673517b1c3bf06d9885505bd2a821
Chloé Legué <chloelegue@gmail.com>
2026-07-15 20:08:36 -0400

    feat: add options to select sound files for toast notifications and enabling the sounds

--- plugin/src/Caelestia/Config/generalconfig.hpp
+++ plugin/src/Caelestia/Config/generalconfig.hpp
@@ -103,6 +103,7 @@ class GeneralBattery : public settings::ObjectNode {
     CONFIG_GLOBAL_PROPERTY(bool, enableHighBatteryWarning, false)
     CONFIG_GLOBAL_PROPERTY(bool, framedMaterialIcons, false)
     CONFIG_GLOBAL_PROPERTY(bool, repeatedWarnings, false)
+    CONFIG_GLOBAL_PROPERTY(bool, toastSound, false)
 
 public:
     explicit GeneralBattery(QObject* parent = nullptr)
