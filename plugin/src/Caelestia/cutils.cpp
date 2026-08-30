a216abaaf4b741e67eef89936d827079f0a82902
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2026-08-30 21:27:02 +1000

    refactor: use default vals instead of overloads

--- plugin/src/Caelestia/cutils.cpp
+++ plugin/src/Caelestia/cutils.cpp
@@ -21,24 +21,8 @@ Q_LOGGING_CATEGORY(lcCUtils, "caelestia.cutils", QtInfoMsg)
 
 namespace caelestia {
 
-void CUtils::saveItem(QQuickItem* target, const QUrl& path) {
-    this->saveItem(target, path, QRect(), QJSValue(), QJSValue());
-}
-
-void CUtils::saveItem(QQuickItem* target, const QUrl& path, const QRect& rect) {
-    this->saveItem(target, path, rect, QJSValue(), QJSValue());
-}
-
-void CUtils::saveItem(QQuickItem* target, const QUrl& path, const QJSValue& onSaved) {
-    this->saveItem(target, path, QRect(), onSaved, QJSValue());
-}
-
 void CUtils::saveItem(QQuickItem* target, const QUrl& path, const QJSValue& onSaved, const QJSValue& onFailed) {
     this->saveItem(target, path, QRect(), onSaved, onFailed);
-}
-
-void CUtils::saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, const QJSValue& onSaved) {
-    this->saveItem(target, path, rect, onSaved, QJSValue());
 }
 
 void CUtils::saveItem(
--- plugin/src/Caelestia/cutils.hpp
+++ plugin/src/Caelestia/cutils.hpp
@@ -17,14 +17,10 @@ class CUtils : public QObject {
     Q_PROPERTY(QString qtVersion READ qtVersion CONSTANT)
 
 public:
-    // clang-format off
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path);
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect);
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QJSValue& onSaved);
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QJSValue& onSaved, const QJSValue& onFailed);
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, const QJSValue& onSaved);
-    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, const QJSValue& onSaved, const QJSValue& onFailed);
-    // clang-format on
+    Q_INVOKABLE void saveItem(
+        QQuickItem* target, const QUrl& path, const QJSValue& onSaved = {}, const QJSValue& onFailed = {});
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, const QJSValue& onSaved = {},
+        const QJSValue& onFailed = {});
 
     Q_INVOKABLE static bool copyFile(const QUrl& source, const QUrl& target, bool overwrite = true);
     Q_INVOKABLE static bool deleteFile(const QUrl& path);
@@ -32,8 +28,7 @@ public:
 
     Q_INVOKABLE static qreal clamp(qreal value, qreal min, qreal max);
 
-    Q_INVOKABLE static QString enumToString(
-        QObject* target, const QString& property, const QVariant& value = QVariant());
+    Q_INVOKABLE static QString enumToString(QObject* target, const QString& property, const QVariant& value = {});
 
     Q_INVOKABLE static QQuickItem* findChild(QQuickItem* root, const QString& name);
     Q_INVOKABLE static QList<QQuickItem*> findChildren(QQuickItem* root, const QString& name);
