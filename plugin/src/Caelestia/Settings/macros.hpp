f4f67687792b0e7ac69fc8ee20847eb2365d0a97
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2026-09-17 14:57:01 +1000

    feat(settings): enforce bounds of union types at compile time
    Also rename tlist -> unionTypes

--- plugin/src/Caelestia/Settings/macros.hpp
+++ plugin/src/Caelestia/Settings/macros.hpp
@@ -16,7 +16,11 @@ inline QVariantMap vmap(std::initializer_list<std::pair<QString, QVariant>> entr
     return map;
 }
 
-template <typename... Ts> inline QList<QMetaType> tlist() {
+template <typename... Ts> inline QList<QMetaType> unionTypes() {
+    static_assert(sizeof...(Ts) >= 2, "A union needs at least two types");
+    static_assert(sizeof...(Ts) <= 4, "A union cannot have more than 4 types");
+    static_assert((!std::is_same_v<Ts, QVariant> && ...), "A union cannot contain QVariant");
+
     return { QMetaType::fromType<Ts>()... };
 }
 
